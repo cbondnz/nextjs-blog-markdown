650c1640629e92ff7880622f1d40ad8147c578de
clayton bond <klaybond@yahoo.co.nz>
2021-11-09 13:29:56 +1300

    added

--- pages/_document.js
+++ pages/_document.js
@@ -56,6 +56,7 @@ MyDocument.getInitialProps = async (ctx) => {
 
   ctx.renderPage = () =>
     originalRenderPage({
+      // eslint-disable-next-line react/display-name
       enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
     });
 
